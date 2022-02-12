6aff44f0e9e57a9cb03590dd9a4dfa83c21d37c6
gobnyond <iong4524@naver.com>
2022-02-12 23:42:30 +0900

    로그인, 회원가입 View 및 button radius extension

---
+++ TDlist/ButtonRadius.swift
@@ -0,0 +1,30 @@
+//
+//  ButtonRadius.swift
+//  TDlist
+//
+//  Created by 정수빈 on 2022/02/12.
+//
+
+import UIKit
+
+class ButtonRadius: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+           didSet{
+           self.layer.cornerRadius = cornerRadius
+           }
+       }
+
+       @IBInspectable var borderWidth: CGFloat = 0{
+           didSet{
+               self.layer.borderWidth = borderWidth
+           }
+       }
+
+       @IBInspectable var borderColor: UIColor = UIColor.clear{
+           didSet{
+               self.layer.borderColor = borderColor.cgColor
+           }
+       }
+
+}
