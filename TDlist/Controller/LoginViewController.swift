11209f3d097895193d15f5ca6698c03be41703a3
gobnyond <iong4524@naver.com>
2022-02-22 23:42:03 +0900

    login api 연결

--- TDlist/Controller/LoginViewController.swift
+++ TDlist/Controller/LoginViewController.swift
@@ -16,7 +16,7 @@ class LoginViewController: UIViewController {
     func postLogin(_ parameters: LoginRequest) {
         AF.request("13.209.10.30:4004/user/login", method: .post, parameters: parameters, encoder: JSONParameterEncoder(), headers: nil)
             .validate()
-            .responseDecodable(of: LoginResponse.self) { [_] response in
+            .responseDecodable(of: LoginResponse.self) { [self] response in
                 switch response.result {
                 case .success(let response):
             
@@ -25,24 +25,35 @@ class LoginViewController: UIViewController {
                        
                     } else {
                         print("로그인 실패")
-                    
-                    }
+                        let alert = UIAlertController(title: "", message: response.message, preferredStyle: .alert)
+                        let okButton = UIAlertAction (title: "확인",style: .default, handler: nil)
+                        alert.addAction(okButton)
+                        self.present(alert, animated:true)
+                  }
                 case .failure(let error):
-                    print("error")
-            }
-        }
+                    print("failure \(error.localizedDescription)")
+              }
+         }
+    
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func loginActionButton(_ sender: UIButton) {
-    }
-    @IBAction func signupActionButton(_ sender: UIButton) {
+        let id = userIdTextfield.text ?? ""
+        let pw = userPwTextfield.text ?? ""
+        
+        let param = LoginRequest(userid: id, userpw: pw)
+            postLogin(param)
     }
     
-
+    @IBAction func signupActionButton(_ sender: UIButton) {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyBoard.instantiateViewController(withIdentifier: "SignUpViewController") as!
+        SignUpViewController
+        self.present(vc, animated: true, completion: nil)
+    }
+    
 }
