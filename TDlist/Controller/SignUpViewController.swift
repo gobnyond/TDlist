db09fb00950aa345ec4d0a8bd1e2b13e93363b7f
gobnyond <iong4524@naver.com>
2022-02-23 22:40:19 +0900

    id duplication error

--- TDlist/Controller/SignUpViewController.swift
+++ TDlist/Controller/SignUpViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 import Alamofire
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var signNameTextfield: UITextField!
     @IBOutlet weak var signIdTextfield: UITextField!
     @IBOutlet weak var signPwTextfield: UITextField!
     @IBOutlet weak var signPwcheckTextfield: UITextField!
+    @IBOutlet weak var idLabel: UILabel!
+    
+    @objc func textFieldDidChange(_ sender: Any?) {
+        signNameTextfield.clearsOnBeginEditing = false
+    }
+    
+    func postIdDuplicate(_ parameters: IdDuplicationRequest) {
+        AF.request("http://13.209.10.30:4004/user/duplicate", method: .post, parameters: parameters, encoder: JSONParameterEncoder(), headers: nil)
+            .validate()
+            .responseDecodable(of: SignUpResponse.self) { [self] response in
+                switch response.result {
+                case .success(let response):
+            
+                    if response.isSuccess == true {
+                        print("사용 가능한 아이디")
+                        self.idLabel?.text = response.message
+                       
+                    } else {
+                        print("아이디 확인 실패")
+                        self.idLabel?.text = response.message
+                  }
+                case .failure(let error):
+                    print("failure \(error.localizedDescription)")
+              }
+         }
+    }
     
     func postSignUp(_ parameters: SignUpRequest) {
         AF.request("http://13.209.10.30:4004/user", method: .post, parameters: parameters, encoder: JSONParameterEncoder(), headers: nil)
@@ -46,6 +72,15 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       // signNameTextfield.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
+        
+        self.signNameTextfield.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        
+        let name = signNameTextfield.text ?? ""
+        signNameTextfield.delegate = self
+        let parameter = IdDuplicationRequest(userid: name)
+            postIdDuplicate(parameter)
 
     }
     
@@ -55,9 +90,15 @@ class SignUpViewController: UIViewController {
         let pw = signPwTextfield.text ?? ""
         let pw_check = signPwcheckTextfield.text ?? ""
         
+        //let parameter = IdDuplicationRequest(userid: name)
+            //postIdDuplicate(parameter)
+        
         let param = SignUpRequest(username: name, userid: id, userpw: pw, userpw_check: pw_check)
             postSignUp(param)
     }
     
   
 }
+                                    
+// func textFieldDidChange (textField: UITextField) {
+// }
