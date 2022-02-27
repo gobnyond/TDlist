d03c04258e63e8a91c48e4c34e4c8ab7644a46c6
gobnyond <iong4524@naver.com>
2022-02-27 23:54:58 +0900

    auto login!

--- TDlist/Controller/LoginViewController.swift
+++ TDlist/Controller/LoginViewController.swift
@@ -12,6 +12,8 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var userIdTextfield: UITextField!
     @IBOutlet weak var userPwTextfield: UITextField!
+    @IBOutlet weak var autoLogin: UISwitch!
+    //autoLogin.transform = CGAffineTransform(scaleX: 0.75, y: 0.75); //스위치 크기
     
     func postLogin(_ parameters: LoginRequest) {
         AF.request("http://13.209.10.30:4004/user/login", method: .post, parameters: parameters, encoder: JSONParameterEncoder(), headers: nil)
@@ -44,25 +46,41 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+        
+        if let save_id:String = UserDefaults.standard.object(forKey: "save_id") as? String,
+           let save_pw:String = UserDefaults.standard.object(forKey: "save_pw") as? String {
     
-    
-    @IBAction func loginAutoButton(_ sender: UIButton) {
-        sender.isSelected = !sender.isSelected
-            //if sender.isSelected == true{
-                //isAutoLogin = true
-            //}else{
-              //  isAutoLogin = false
-            //}
+        let param = LoginRequest(userid: save_id, userpw: save_pw)
+            postLogin(param)
         }
-    
-    
+        
+    }
+
     @IBAction func loginActionButton(_ sender: UIButton) {
+        
         let id = userIdTextfield.text ?? ""
         let pw = userPwTextfield.text ?? ""
         
+        if autoLogin.isOn{ //스위치가 켜져있을때
+                    let dataSave = UserDefaults.standard // UserDefaults.standard 정의
+                    dataSave.setValue(id, forKey: "save_id") // save_userNm 키값에 id값 저장
+                    dataSave.setValue(pw, forKey: "save_pw") // save_pw 키값에 pw값 저장
+                     
+                    UserDefaults.standard.synchronize() // setValue 실행
+                } else { // 스위치가 꺼져있을때
+                    let dataSave = UserDefaults.standard
+                    dataSave.setValue("nil", forKey: "save_id")
+                    dataSave.setValue("nil", forKey: "save_pw")
+                     
+                    UserDefaults.standard.synchronize()
+                }
+                 
+                print("\(UserDefaults.standard.value(forKey: "save_id")!)")
+                print("\(UserDefaults.standard.value(forKey: "save_pw")!)")
+        
         let param = LoginRequest(userid: id, userpw: pw)
             postLogin(param)
+        
     }
     
     @IBAction func signupActionButton(_ sender: UIButton) {
